cc4d5a68da0d14a17038b265da1146273ef9a6b8
Ahmad Mustapha <18-30gc052@students.unilorin.edu.ng>
2023-07-06 23:54:49 +0100

    Settup Database connection string and Setup a router for the user creation endpoint

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/dranonymousnet/rss-agg/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
 
 func main(){
 
@@ -18,6 +26,20 @@ func main(){
 	if portString == ""{
 		log.Fatal("PORT is not found in the environment")
 	}
+
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == ""{
+		log.Fatal("Unable to retrieve Database URL in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Unable to connect to database")
+	}
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	} 
 
 	log.Printf("Server starting on %v", portString)
 	router := chi.NewRouter()
@@ -40,6 +62,7 @@ func main(){
 	)
 	v1Router.Get("/err", handlerError,
 	)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount(
 		"/v1", v1Router,
@@ -49,7 +72,7 @@ func main(){
 		Handler: router,
 		Addr : ":"+portString,
 	}
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err == nil{
 		log.Fatal(err)
 	}
